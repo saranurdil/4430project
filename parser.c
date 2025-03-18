37ce235614e2d64f9f64f2e99a526802916a74d1
Sara Nurdil <sn1582@cell05-cse.unt.ad.unt.edu>
2025-03-18 00:19:01 -0500

    Added a NodeType enum and ASTNode struct

--- parser.c
+++ parser.c
@@ -2,3 +2,22 @@
 #include <string.h>
 #include <stdlib.h>
 
+//node type options
+typedef enum {
+    NODE_INTEGER, 
+    NODE_ADD,
+    NODE_SUBTRACT,
+    NODE_MULTIPLY,
+    NODE_DIVIDE
+} NodeType;
+
+//Node struct
+typedef struct ASTNode {
+    NodeType type;
+    int value; //equals to zero for non-integer type nodes
+    struct ASTNode* left;
+    struct ASTNode* right;
+} ASTNode;
+
+
+
