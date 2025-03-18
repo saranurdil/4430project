13519b454fe208aed6d81c09eec675b75fdae143
Sara Nurdil <sn1582@cell05-cse.unt.ad.unt.edu>
2025-03-17 20:43:45 -0500

    Declared grammar functions

--- parser.c
+++ parser.c
@@ -20,3 +20,30 @@ TreeNode* createNode(char* value) {
     node->right = NULL;
     return node;
 }
+
+//GRAMMAR FUNCTIONS:
+
+TreeNode* expression(){
+    
+}
+TreeNode* expression_suffix(){
+
+}
+TreeNode* term(){
+
+}
+TreeNode* term_suffix(){
+
+}
+TreeNode* factor(){
+
+}
+TreeNode* integer(){
+
+}
+TreeNode* integer_suffix(){
+
+}
+TreeNode* digit(){
+
+}
