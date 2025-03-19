0063b5e075a610310c4387e3ff0ea848e84c322d
JudeQ <judequbbaj@my.unt.edu>
2025-03-19 18:15:42 -0500

    Added syntax error handling and updated AST logic and README files.

--- README.md
+++ README.md
@@ -1,34 +1,45 @@
-# CSCE 4430
-# Term Project 1
-# Brooke Mesch
-# Sara Nurdil
-To compile and run the project together with the parser enter the following commands in this order (Linux):
+# CSCE 4430 - Term Project 1
+# By Brooke Mesch, Sara Nurdil, Jude Qubbaj
+
+## Project Description
+This project is a lexical analyzer implemented using Flex. It tokenizes simple arithmetic expressions consisting of integers, operators, and parentheses. Before running the lexer, ensure your input.txt file(s) already exist. The output is printed to the console, redirect as needed. 
+
+## Compilation Instructions
+### 1. Compile the Lexer
+**Linux/macOS:**
+flex lexer.l gcc les.yy.c -o lexer
+
+**Windows:**
+flex lexer.l gcc lex.yy.c -o lexer.exe
+
+### 2. Compile the Parser
+**Linux/macOS:**
+gcc parser.c -o parser
+
+**Windows:**
+gcc parser.c -o parser.exe
+
+## Execution Instructions
+### 3. Run the Lexer
+Run the lexer to tokenize the input file:
+**Linux/macOS:**
+./lexer input.txt > tokens.txt 
+**Windows:**
+ lexer.exe input.txt > tokens.txt 
+
+### 4. Run the Parser
+Parse the tokenized output to construct the Abstract Syntax Tree:
+**Linux/macOS:**
+./parser < tokens.txt
+
+**Windows:**
+parser.exe < tokens.txt
+
+## To compile and run the project together with the parser enter the following commands in this order (Linux):
 ```
+
 flex lexer.l
 gcc lex.yy.c -o lexer
 gcc parser.c -o parser
 ./lexer input.txt | ./parser
 ```
-
-# Jude Qubbaj
-
-
-This project is a lexical analyzer implemented using Flex. It tokenizes simple arithmetic expressions consisting of integers, operators, and parentheses. Before running the lexer, ensure your input.txt file(s) already exist. The output is printed to the console, redirect as needed. 
-
-To Run:
-
-Windows: 
-Open cmd
-Run the following commands: 
-flex lexer.l
-gcc.lex.yy.c -o lexer.exe
-lexer.exe input.txt
-
-Linux/Mac
-Open the terminal
-Run the following commands:
-flex lexer.l
-gcc.lex.yy.c -o lexer
-./lexer input.txt
-
-
--- parser.c
+++ parser.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-//node type options
+// Node type options
 typedef enum {
     NODE_INTEGER, 
     NODE_ADD,
@@ -11,10 +11,10 @@ typedef enum {
     NODE_DIVIDE
 } NodeType;
 
-//Node struct
+// Node struct
 typedef struct ASTNode {
     NodeType type;
-    int value; //equals to zero for non-integer type nodes
+    int value; // Equals to zero for non-integer type nodes
     struct ASTNode* left;
     struct ASTNode* right;
 } ASTNode;
@@ -28,107 +28,115 @@ ASTNode* factor();
 char current_token[100];
 char current_token_type[100];
 
-//function that creates new nodes for a tree
-ASTNode* create_node(NodeType type, int value, ASTNode* left, ASTNode* right){
-
-    ASTNode* node = (ASTNode*)malloc(sizeof(ASTNode)); //allocate memory for the node
-    node->type = type; //set node type (i.e integer, add, subtract, etc)
-    node->value = value; //set node value (i.e 15, +, /, -, etc)
-    node->left = left; //set node's left child
-    node->right = right; //set node's right child
-
+// Function that creates new nodes for a tree
+ASTNode* create_node(NodeType type, int value, ASTNode* left, ASTNode* right) {
+    ASTNode* node = (ASTNode*)malloc(sizeof(ASTNode)); 
+    node->type = type;
+    node->value = value;
+    node->left = left;
+    node->right = right;
     return node;
 }
 
 // Function to get the next token from the lexer output
 void get_next_token() {
-    if(scanf("%90[^,],%90s", current_token, current_token_type) != 2){ //scan for the token (e.g 2) and its type (e.g. integer)
-        // if scanf did not succeed, clear the current token and its type
+    if(scanf("%90[^,],%90s", current_token, current_token_type) != 2) {
         strcpy(current_token, "");
         strcpy(current_token_type, "");
     }
 }
 
 // GRAMMAR: <expression> -> <term> <expression_suffix>
-ASTNode* expression(){
+ASTNode* expression() {
     ASTNode* term_node = term();
     return expression_suffix(term_node);
 }
 
 // GRAMMAR: <term> -> <factor> <term_suffix>
-ASTNode* term(){
+ASTNode* term() {
     ASTNode* factor_node = factor();
     return term_suffix(factor_node);
 }
 
 // GRAMMAR: <factor> -> ( <expression> ) | <integer>
-ASTNode* factor(){
-    //check if current token is "(" -> call expression()
-    if(strcmp(current_token_type, "left_paren") == 0){
-        get_next_token(); //move to the next token (which is the beginning of our expression)
-        ASTNode* inner_expr = expression(); //call expression
+ASTNode* factor() {
+    // Check if current token is "(" -> call expression()
+    if (strcmp(current_token_type, "left_paren") == 0) {
+        get_next_token();  // Move to the next token (which should be an expression)
+        ASTNode* inner_expr = expression();  // Call expression
 
-        get_next_token(); 
+        if (strcmp(current_token_type, "right_paren") != 0) {
+            printf("Error: Expected ')' but found '%s'\n", current_token);
+            exit(1);
+        }
+        get_next_token();
+
         return inner_expr;
-    }
-    else if (strcmp(current_token_type, "integer") == 0){
+    } 
+    else if (strcmp(current_token_type, "integer") == 0) {
         int value = atoi(current_token);
         ASTNode* node = create_node(NODE_INTEGER, value, NULL, NULL);
         get_next_token();
         return node;
+    } 
+    else {
+        printf("Error: Unexpected token '%s' of type '%s'\n", current_token, current_token_type);
+        exit(1);
     }
-    //check if current token is and integer
 }
 
 // GRAMMAR: <term_suffix> -> * <factor> <term_suffix> | / <factor> <term_suffix> | ε
-ASTNode* term_suffix(ASTNode* left){
-    //check if current token is * or /
-    if(strcmp(current_token_type, "multiply_operator") == 0){
-        get_next_token();
+ASTNode* term_suffix(ASTNode* left) {
+    // Check if current token is * or /
+    if (strcmp(current_token_type, "multiply_operator") == 0 || strcmp(current_token_type, "divide_operator") == 0) {
+        char operator_type[100]; 
+        strcpy(operator_type, current_token_type);  
+
+        get_next_token();  
+
         ASTNode* factor_node = factor();
-        ASTNode* new_node = create_node(NODE_MULTIPLY, 0, left, factor_node);
+        ASTNode* new_node = (strcmp(operator_type, "multiply_operator") == 0) ?
+                            create_node(NODE_MULTIPLY, 0, left, factor_node) :
+                            create_node(NODE_DIVIDE, 0, left, factor_node);
+
         return term_suffix(new_node);
     }
-    else if (strcmp(current_token_type, "divide_operator") == 0){
-        get_next_token();
-        ASTNode* factor_node = factor();
-        ASTNode* new_node = create_node(NODE_DIVIDE, 0, left, factor_node);
-        return term_suffix(new_node);
-    }
-    else{
-        return left;
-    }
+    return left;
 }
+
 // GRAMMAR: <expression_suffix> -> + <term> <expression_suffix> | - <term> <expression_suffix> | ε
-ASTNode* expression_suffix(ASTNode* left){
-    //check if current token is + or -
-    if(strcmp(current_token_type, "add_operator") == 0){
-        get_next_token();
-        ASTNode* factor_node = term();
-        ASTNode* new_node = create_node(NODE_ADD, 0, left, factor_node);
-        return expression_suffix(new_node);
+ASTNode* expression_suffix(ASTNode* left) {
+    // Check if current token is + or -
+    if (strcmp(current_token_type, "add_operator") == 0 || strcmp(current_token_type, "subtract_operator") == 0) {
+        char operator_type[100]; 
+        strcpy(operator_type, current_token_type);  
+
+        get_next_token();  
+
+        ASTNode* right = expression();  
+
+        // Build AST tree
+        ASTNode* new_node = (strcmp(operator_type, "add_operator") == 0) ?
+                            create_node(NODE_ADD, 0, left, right) :
+                            create_node(NODE_SUBTRACT, 0, left, right);
+
+        return new_node;  
     }
-    else if (strcmp(current_token_type, "subtract_operator") == 0){
-        get_next_token();
-        ASTNode* factor_node = term();
-        ASTNode* new_node = create_node(NODE_SUBTRACT, 0, left, factor_node);
-        return expression_suffix(new_node);
-    }
-    else{
-        return left;
-    }
+    return left;
 }
 
-void print_tree(ASTNode* node, int indent){
+// Function to print the AST
+void print_tree(ASTNode* node, int indent) {
 
-    //if the tree is empty
-    if (node == NULL){
+    // If the tree is empty
+    if (node == NULL) {
         printf("Empty tree.\n");
         return;
     }
 
-    for(int i = 0; i < indent; i++) printf("\t");
-    switch(node->type){
+    for (int i = 0; i < indent; i++) printf("\t");
+    
+    switch (node->type) {
         case NODE_INTEGER:
             printf("%d (int)\n", node->value);
             break;
@@ -137,8 +145,8 @@ void print_tree(ASTNode* node, int indent){
             print_tree(node->left, indent + 1);
             print_tree(node->right, indent + 1);
             break;
-        case NODE_DIVIDE:
-            printf("/ (Divide)\n");
+        case NODE_SUBTRACT:
+            printf("- (Subtract)\n");
             print_tree(node->left, indent + 1);
             print_tree(node->right, indent + 1);
             break;
@@ -147,19 +155,18 @@ void print_tree(ASTNode* node, int indent){
             print_tree(node->left, indent + 1);
             print_tree(node->right, indent + 1);
             break;
-        case NODE_SUBTRACT:
-            printf("- (Subtract)\n");
+        case NODE_DIVIDE:
+            printf("/ (Divide)\n");
             print_tree(node->left, indent + 1);
             print_tree(node->right, indent + 1);
             break;
     }
-
 }
-int main(){
+
+// Main function
+int main() {
     get_next_token();
     ASTNode* root = expression();
     print_tree(root, 0);
-
     return 0;
-
 }
